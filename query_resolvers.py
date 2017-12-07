7054e21581200488facbd911762b8b82ee3e9bcd
lthati <lthati@opendns.com>
2017-12-07 13:57:13 -0800

    get all IPs

--- query_resolvers.py
+++ query_resolvers.py
@@ -1,12 +1,19 @@
 import sys
 import requests
+import socket
+import dns.resolver
 
-def main(hostname):
+def main(argv):
 	inventory = requests.get('https://inventory.p1.opendns.com/v1/inventory.json').json()
-	resolvers = [inventory["datacenters"][0]["serviceHosts"]["resolvers"]["m1.ams"]["ipv4_address"]]
-	for resolver in resolvers:
-		print resolver
-
+	resolver_ips = [inventory["datacenters"][0]["serviceHosts"]["resolvers"]["m1.ams"]["ipv4_address"]]
+	resolver = dns.resolver.Resolver()
+	server_ips = set()
+	for resolver_ip in resolver_ips:
+		resolver.nameservers=[resolver_ip]
+		for rdata in resolver.query("internetbadguys.com"):
+			 server_ips.add(rdata)
+	for ip in server_ips:
+		print ip
 
 if __name__ == "__main__":
     main(sys.argv)
